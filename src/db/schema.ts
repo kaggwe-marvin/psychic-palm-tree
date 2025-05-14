7af7db04b0dcf21418ecddfa489789a59c597774
kaggwe marvin victor <kiwanukamarvin597@gmail.com>
2025-05-14 15:56:57 +0300

    feat: Update database schema by removing unused fields and adding new migration files

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,31 +8,11 @@ export const users = sqliteTable('users', {
   email: text('email').notNull().unique(),
   password: text('password').notNull(),
   role: text('role').notNull(),
-  name: text('name'),
-  department: text('department'),
-  position: text('position'),
-  phone: text('phone'),
-  
-  lastLogin: integer('last_login'),
-  createdAt: integer('created_at').$defaultFn(() => Math.floor(Date.now() / 1000)),
 });
 
-export const departments = sqliteTable('departments', {
-  id: text('id').primaryKey().notNull().$defaultFn(()=> generateId(15)),
-  name: text('name').notNull(),
-  code: text('code').notNull().unique(),
-  description: text('description'),
-  headOfDepartment: text('head_of_department'),
-  email: text('email'),
-  phone: text('phone'),
-  location: text('location'),
-  createdAt: integer('created_at').$defaultFn(() => Math.floor(Date.now() / 1000)),
-});
 
 export type SelectUser = typeof users.$inferSelect;
 export type InsertUser = typeof users.$inferInsert;
-export type SelectDepartment = typeof departments.$inferSelect;
-export type InsertDepartment = typeof departments.$inferInsert;
 
 export const sessions = sqliteTable('sessions', {
   id: text('id').primaryKey().notNull(),
